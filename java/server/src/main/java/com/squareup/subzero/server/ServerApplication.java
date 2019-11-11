fb7d1b46a33425ca8f25bcbc628383d0eeaf639d
Alok Menghrajani <alok@squareup.com>
2019-11-11 13:43:37 -0800

    Improve error handling in debug server
    Previously, we were silently ignoring any exception thrown by the
    backend. With this change, an error message gets rendered.
    
    Better error handling implies we can remove two unnecessary try/catch
    blocks and just let the exception propagate.
    
    Finally replaces a null with an exception.
    
    The combination of these makes situations such as
    https://github.com/square/subzero/issues/119 easier to debug.

--- java/server/src/main/java/com/squareup/subzero/server/ServerApplication.java
+++ java/server/src/main/java/com/squareup/subzero/server/ServerApplication.java
@@ -1,13 +1,14 @@
 package com.squareup.subzero.server;
 
 import com.squareup.subzero.server.resources.AssetsResource;
+import com.squareup.subzero.server.resources.ComputeResource;
 import com.squareup.subzero.server.resources.ConstantsResource;
 import com.squareup.subzero.server.resources.GenerateQrCodeResource;
 import com.squareup.subzero.server.resources.PrettyPrintResource;
-import com.squareup.subzero.server.resources.ComputeResource;
 import com.squareup.subzero.server.resources.ShowFinalTransactionResource;
 import io.dropwizard.Application;
 import io.dropwizard.assets.AssetsBundle;
+import io.dropwizard.jersey.setup.JerseyEnvironment;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
@@ -29,11 +30,13 @@ public class ServerApplication extends Application<ServerConfiguration> {
 
     @Override
     public void run(final ServerConfiguration configuration, final Environment environment) {
-        environment.jersey().register(new AssetsResource());
-        environment.jersey().register(new ConstantsResource());
-        environment.jersey().register(new PrettyPrintResource());
-        environment.jersey().register(new GenerateQrCodeResource());
-        environment.jersey().register(new ComputeResource());
-        environment.jersey().register(new ShowFinalTransactionResource());
+        JerseyEnvironment jersey = environment.jersey();
+        jersey.register(new AssetsResource());
+        jersey.register(new ConstantsResource());
+        jersey.register(new PrettyPrintResource());
+        jersey.register(new GenerateQrCodeResource());
+        jersey.register(new ComputeResource());
+        jersey.register(new ShowFinalTransactionResource());
+        jersey.register(new ServerExceptionMapper());
     }
 }
