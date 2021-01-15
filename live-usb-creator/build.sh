ed5deeead5fac4bcdd2131012b1640886e26c015
Ning Shang <nshang@squareup.com>
2021-01-14 21:05:48 -0800

    Differentiate ISO types: dev or release Release build: '/vagrant/build.sh' or '/vagrant/build.sh release'. Development build: '/vagrant/build.sh dev'.
    A release image is to be used by cold storage operators who do not need to
    have access to an interactive shell.  For a release image to boot into a
    root shell successfully, an SCSI/ATA hard drive needs to be available, with
    a partition '/dev/sda1' used as persistent storage for the cold wallet. The
    partition can be formatted in ext3 or vfat for example. The device node
    '/dev/sda1' is required to give the operators a seamless experience when
    booting up the system. A few other files, including the HSM world and
    encrypted secrets, and compiled subzero code (in .jar and signed .sar), are
    not checked in to the project repository, and need to be added to their
    appropriate locations (please refer to README.md) before building a release
    image that will work out of the box.
    
    A development image is to be used by subzero developers for HSM and cold
    wallet development. A developer can boot into an interactive root shell
    without performing the above mentioned steps. The HSM software is
    automatically installed in the development image, but a developer needs to
    manually set up persistent storage for the cold wallet, put .jar and
    .sar files to appropriate filesystem locations, and execute them
    accordingly, for development and testing.

--- live-usb-creator/build.sh
+++ live-usb-creator/build.sh
@@ -10,13 +10,30 @@ sudo cp /vagrant/patches/usr/share/lorax/live/x86.noeverything.tmpl /usr/share/l
 sudo cp /vagrant/patches/usr/share/lorax/live/efi.tmpl /usr/share/lorax/live/efi.tmpl
 
 ###############################################################################
+# Customize live scripts based on build type: release (default) or dev
+###############################################################################
+rm -rf /vagrant/live_scripts/.isotype_rel /vagrant/live_scripts/.isotype_dev
+if [ "$1" = "dev" ]; then
+  BUILD_TYPE="Dev"
+  cp /vagrant/live_scripts/install_nfast_tools_dev /vagrant/live_scripts/install_nfast_tools
+  touch /vagrant/live_scripts/.isotype_dev
+elif [ "$1" = "release" ] || [ "$#" -eq 0 ]; then
+  BUILD_TYPE="Rel"
+  cp /vagrant/live_scripts/install_nfast_tools_release /vagrant/live_scripts/install_nfast_tools
+  touch /vagrant/live_scripts/.isotype_rel
+else
+  echo "Error occurred. Usage: $0 [release|dev]." >&2
+  exit 1
+fi
+
+##############################################################################
 # Actually build the ISO
 ###############################################################################
 
 # livemedia-creator refuses to run if results_dir exists
 sudo rm -rf /tmp/build
 
-sudo livemedia-creator --logfile=/vagrant/livemedia-creator.log --make-iso --ks=/vagrant/rhel7-livemedia.ks --resultdir="/tmp/build" --no-virt --project="CentOS" --releasever="7.7.1908" --volid="CentOS 7 (1908) + nCipher x86_64"
+sudo livemedia-creator --logfile=/vagrant/livemedia-creator.log --make-iso --ks=/vagrant/rhel7-livemedia.ks --resultdir="/tmp/build" --no-virt --project="CentOS" --releasever="7.7.1908" --volid="CentOS 7 (1908) + nCipher ($BUILD_TYPE)"
 
 ###############################################################################
 # Copy ISO back to host
