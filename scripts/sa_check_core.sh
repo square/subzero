c457d89b8d237e8365344650ea0fcd72aced6429
Ning Shang <nshang@squareup.com>
2021-02-24 12:52:22 -0800

    Static analysis of subzero CORE a la clang static analyzer This commit only adds a script for clang static analyzer based static analysis. CI integration will be done as a follow-up commit.

---
+++ scripts/sa_check_core.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# clang-check (included in LLVM tools) needs to be installed for 
+# static analysis on subzero CORE a la clang static analyzer
+
+set -euxo pipefail
+
+echo "Static analysis through clang static analyzer"
+EXCLUDE_PATH_PATTERN="\/external-crypto\/\|\/trezor-crypto\/"
+SUBZERO_ROOT=$(cd "$(dirname "$0")"; pwd -P)
+cd "$(dirname $0)"/..
+mkdir -p core/build
+cd core/build
+TARGET=dev CURRENCY=btc-testnet cmake ../ -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+
+COMPILE_DB_DIR="${SUBZERO_ROOT}/../core/build"
+COMPILE_DB="${COMPILE_DB_DIR}/compile_commands.json"
+if [ ! -f "${COMPILE_DB}" ]; then
+    exit 1
+fi
+
+grep file "${COMPILE_DB}" |
+awk '{ print $2; }' |
+sed 's/\"//g' |
+while read FILE; do
+  if ! grep -q ${EXCLUDE_PATH_PATTERN} <<< "${FILE}"; then
+    cd $(dirname ${FILE});
+    output=$(clang-check -analyze -extra-arg -Xanalyzer -extra-arg \
+      -analyzer-disable-checker=unix \
+      -extra-arg -Xanalyzer -extra-arg \
+      -analyzer-checker=core,deadcode,security,cplusplus,nullability \
+      -extra-arg -Xanalyzer -extra-arg -analyzer-output=text \
+      -p ${COMPILE_DB_DIR} \
+      $(basename ${FILE}) 2>&1)
+    if grep -q "warning:" <<< "${output}"; then
+      >&2 echo ${output}
+      exit 1
+    fi
+  fi
+done
+echo "Static analysis returns clean result"
