44dafb608f52a5fd2edbc227f1e09bc8bce21517
Ning Shang <nshang@squareup.com>
2021-02-25 20:46:34 -0800

    Update Codesafe toolchain to 12.63.0 Security world version stays at 12.50.2. Codesafe version is upgraded to 12.63.0. We will need a new vendor ISO Codesafe_Lin64-12.63.0.iso for this upgrade.

--- live-usb-creator/bootstrap.sh
+++ live-usb-creator/bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# mount the SecWorld + CodeSafe image so we can copy stuff off it to our target image
-sudo mkdir /media/SecWorld && sudo mount -t iso9660 -o ro /vagrant/SecWorld_Lin64-12.60.11.iso /media/SecWorld
+# mount the SecWorld (12.50.2) + CodeSafe (12.63.0) image so we can copy stuff off it to our target image
+sudo mkdir /media/SecWorld && sudo mount -t iso9660 -o ro /vagrant/CodeSafe-linux64-dev-12.50.2.iso /media/SecWorld
 sudo mkdir /media/CodeSafe && sudo mount -t iso9660 -o ro /vagrant/Codesafe_Lin64-12.63.0.iso  /media/CodeSafe
