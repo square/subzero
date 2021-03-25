bae9639b8151ab678283c70867794da20bfc4ed6
Ning Shang <nshang@squareup.com>
2021-03-24 17:57:38 -0700

    Clarify M_KeyID typedef for TARGET=dev
    Thank jknutsen-cashapp for the feedback.

--- core/include/aes_gcm_dev.h
+++ core/include/aes_gcm_dev.h
@@ -4,6 +4,12 @@
  * dev implementation */
 
 // M_KeyID typedef for the dev target
+//
+// Note that it's a re-definition of the codesafe-specific M_KeyID type. M_keyID
+// is an integer typed handle to an HSM key object. The M_KeyID value 0 is
+// reserved for an invalid key object in the nCipher target. We would like to
+// simulate this behavior in the dev target, and therefore avoid value 0 for
+// M_KeyID enum below.
 typedef enum {master_seed_encryption_key = 1,
               pub_key_encryption_key = 2} M_KeyID;
 
