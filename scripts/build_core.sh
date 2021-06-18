be294259df774bd5dd5dc889201c9beadf644a49
Ning Shang <nshang@squareup.com>
2021-06-17 22:42:41 -0700

    Fix signtx GHA workflow Use subzero_testnet as core's name, to match the changes in build_core.sh.

--- scripts/build_core.sh
+++ scripts/build_core.sh
@@ -5,12 +5,12 @@ set -euxo pipefail
 cd "$(dirname $0)"/..
 mkdir -p core/build
 cd core/build
-# For testnet. Generate compile_db for clang static analyzer
-TARGET=dev CURRENCY=btc-testnet cmake ../ -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
-make
-mv subzero subzero_testnet
 # For mainnet. Skip compile_db generation
-make clean
 TARGET=dev CURRENCY=btc-mainnet cmake ../
 make
 mv subzero subzero_mainnet
+# For testnet. Generate compile_db for clang static analyzer
+make clean
+TARGET=dev CURRENCY=btc-testnet cmake ../ -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+make
+mv subzero subzero_testnet
