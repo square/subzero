89a711a1a1e57adf62abb5f8580876b455b87679
Nitin Mahendru <nmahendru@squareup.com>
2021-09-08 12:50:43 -0700

    Qr Code signature checking.
    These changes are backward compatible.
    Old coordinator service works just fine.
    
    5 new test cases for QR signing for the checked in key have been added.
    The serialized value generated at the coordinator service is
    passed allong on the wire uptil the subzero core.
    
    subzero core will deserialize and check a signature on bytes if present.
    If the signature is not verified then you get QRSIG_CHECK_FAILED.(new
    error code)

--- java/gui/src/main/java/com/squareup/subzero/actions/SignTx.java
+++ java/gui/src/main/java/com/squareup/subzero/actions/SignTx.java
@@ -106,6 +106,10 @@ public class SignTx {
     signTx.addAllOutputs(command_request.getSignTx().getOutputsList());
     signTx.setLockTime(command_request.getSignTx().getLockTime());
 
+    signTx.addAllInputs(command_request.getSignTx().getInputsList());
+    signTx.addAllOutputs(command_request.getSignTx().getOutputsList());
+    signTx.setLockTime(command_request.getSignTx().getLockTime());
+
 
     NCipher nCipher = null;
     if (subzero.nCipher) {
