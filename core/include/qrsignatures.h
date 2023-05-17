4dcb2143a0121d9f1d8eccd5e3aebb7e651ec250
Ilya Maykov <ilyam@squareup.com>
2023-05-17 11:32:06 -0700

    [core] check bounds of signature and pubkey inputs in qrsignature.c
    This wasn't checked before so it was possible to pass in a short signature from
    outside, which would cause an out-of-bounds read in the signature verification code.
    Probably not a security vulnerability, but technically UB so better not risk it.

--- core/include/qrsignatures.h
+++ core/include/qrsignatures.h
@@ -4,20 +4,42 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#define QRSIGNATURE_LEN ((size_t) 64)
+#define QRSIGNATURE_PUBKEY_LEN ((size_t) 65)
+
 /**
  * signature verification wrapper for trezor crypto util.
  * Input should be raw bytes(unhashed). This function will hash input with sha256
  * before verification.
+ *
+ * Preconditions:
+ * - None of the pointers may be NULL.
+ * - data_len must be > 0.
+ * - signature_len must equal QRSIGNATURE_LEN.
  * 
  * @return true on successful signature verification and false otherwise.
  */
-bool check_qrsignature(const uint8_t * const data, size_t data_len, const uint8_t * const signature);
+bool check_qrsignature(
+    const uint8_t* const data,
+    const size_t data_len,
+    const uint8_t* const signature,
+    const size_t signature_len);
 
 /**
  * Same as above.
  * You get to choose the public key to verify sig with.
  * So it can be used in self checks.
+ *
+ * Preconditions:
+ * - None of the pointers may be NULL.
+ * - data_len must be > 0.
+ * - signature_len must equal QRSIGNATURE_LEN.
+ * - pubkey_len must equal QRSIGNATURE_PUBKEY_LEN
  */
-bool check_qrsignature_pub(const uint8_t * const data, size_t data_len, const uint8_t * const signature, const uint8_t * const pubkey);
-
-
+bool check_qrsignature_pub(
+    const uint8_t* const data,
+    const size_t data_len,
+    const uint8_t* const signature,
+    const size_t signature_len,
+    const uint8_t* const pubkey,
+    const size_t pubkey_len);
--- core/src/qrsignatures.c
+++ core/src/qrsignatures.c
@@ -6,37 +6,56 @@
 #include "log.h"
 #include "nist256p1.h"
 
+#include <assert.h>
 #include <stdbool.h>
 
-static const uint8_t QR_PUBKEY[65] = _QR_PUBKEY;
-bool check_qrsignature(const uint8_t * const data, size_t data_len, const uint8_t * const signature){
-  return check_qrsignature_pub(data, data_len, signature, QR_PUBKEY);
+static const uint8_t QR_PUBKEY[QRSIGNATURE_PUBKEY_LEN] = _QR_PUBKEY;
+
+bool check_qrsignature(
+    const uint8_t* const data,
+    const size_t data_len,
+    const uint8_t* const signature,
+    const size_t signature_len) {
+  static_assert(sizeof(QR_PUBKEY) == QRSIGNATURE_PUBKEY_LEN, "sizeof(QR_PUBKEY) must equal QRSIGNATURE_PUBKEY_LEN");
+  return check_qrsignature_pub(data, data_len, signature, signature_len, QR_PUBKEY, sizeof(QR_PUBKEY));
 }
 
-bool check_qrsignature_pub(const uint8_t * const data, size_t data_len, const uint8_t * const signature, const uint8_t * const pub){
-
-  if (data_len == 0){
+bool check_qrsignature_pub(
+    const uint8_t* const data,
+    const size_t data_len,
+    const uint8_t* const signature,
+    const size_t signature_len,
+    const uint8_t* const pubkey,
+    const size_t pubkey_len) {
+  if (data_len == 0) {
     ERROR("Input length is zero.");
     return false;
   }
-  if (data == NULL){
+  if (data == NULL) {
     ERROR("Input data is null.");
     return false;
   }
-  if(signature == NULL){
+  if (signature_len != QRSIGNATURE_LEN) {
+    ERROR("%s: signature_len is: %zu, expected: %zu", __func__, signature_len, QRSIGNATURE_LEN);
+    return false;
+  }
+  if (signature == NULL) {
     ERROR("Signature is null");
     return false;
   }
-  if (pub == NULL) {
+  if (pubkey_len != QRSIGNATURE_PUBKEY_LEN) {
+    ERROR("%s: pubkey_len is: %zu, expected: %zu", __func__, pubkey_len, QRSIGNATURE_PUBKEY_LEN);
+    return false;
+  }
+  if (pubkey == NULL) {
     ERROR("pub is null.");
     return false;
   }
 
-  
   int result_verify = ecdsa_verify(
     (const ecdsa_curve *)&nist256p1,
     HASHER_SHA2,
-    (const uint8_t *)pub,
+    (const uint8_t *) pubkey,
     signature,
     data,
     (uint32_t)data_len);
