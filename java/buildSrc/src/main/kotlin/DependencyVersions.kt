995463b0c83e5aca1ab7b03016358af36170db3c
Ilya Maykov <ilyam@squareup.com>
2023-09-19 15:20:35 -0700

    [java] update guava to 32.0.1-jre
    The guava 31.1-jre dependency has 2 CVEs, which are fixed in 32.0.1-jre.
    This version was also already a transitive dependency via dropwizard, so
    depending on it directly doesn't actually introduce any new dependencies.

--- java/buildSrc/src/main/kotlin/DependencyVersions.kt
+++ java/buildSrc/src/main/kotlin/DependencyVersions.kt
@@ -4,7 +4,7 @@ object DependencyVersions {
   val bitcoinj = "0.16.1"
   val dropwizard = "2.1.7"
   val findbugs = "3.0.2"
-  val guava = "31.1-jre"
+  val guava = "32.0.1-jre"
   val junit = "4.13.2"
   val protobuf = "3.21.7"
   val snakeyaml = "2.0"
