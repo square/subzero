72c1c5a45dc4fc9d28c47606848da509a6e455c7
Ilya Maykov <ilyam@squareup.com>
2024-11-13 12:10:13 -0800

    Update dependencies to mitigate known vulnerabilities
    BitcoinJ: 0.16.1 -> 0.16.3
    Dropwizard: 2.1.7 -> 2.1.12
    Protobuf: 3.21.7 -> 3.25.5
    Bouncycastle: 1.76 -> 1.78
    
    All vulnerabilities were detected using IntelliJ's vulnerability scanner.
    
    This gets rid of all the high-severity and a few of the medium-severity vulnerabilities without breaking compatibility.

--- java/buildSrc/src/main/kotlin/DependencyVersions.kt
+++ java/buildSrc/src/main/kotlin/DependencyVersions.kt
@@ -1,13 +1,13 @@
 package subzero
 
 object DependencyVersions {
-  val bitcoinj = "0.16.1"
-  val dropwizard = "2.1.7"
+  val bitcoinj = "0.16.3"
+  val dropwizard = "2.1.12"
   val findbugs = "3.0.2"
   val guava = "32.0.1-jre"
   val junit = "4.13.2"
-  val protobuf = "3.21.7"
+  val protobuf = "3.25.5"
   val snakeyaml = "2.0"
-  val bouncycastle = "1.76"
+  val bouncycastle = "1.78"
   val zxing = "3.5.0"
 }
